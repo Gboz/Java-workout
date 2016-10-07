d283f583c1ad08a1d80317cbdff12d938ecde46f
Gboz <gbolesta21@gmail.com>
2016-10-07 22:42:40 +0200

    New class FRastragina, build code which generated matrix binary numbers, used Random and Scanner classes.

---
+++ src/com/gboz/javaworkout/FRastragina.java
@@ -0,0 +1,10 @@
+package com.gboz.javaworkout;
+
+import java.util.Random;
+
+/**
+ * Created by Grzegorz on 07.10.2016.
+ */
+public class FRastragina {
+
+}
--- src/com/gboz/javaworkout/Main.java
+++ src/com/gboz/javaworkout/Main.java
@@ -1,10 +1,28 @@
 package com.gboz.javaworkout;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Use method from class KeyWords");
-        KeyWords var = new KeyWords();
-        var.KeyWordsOutput(var.keyWords);
+//        System.out.println("Use method from class KeyWords");
+//        KeyWords var = new KeyWords();
+//        var.KeyWordsOutput(var.keyWords);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        Random random = new Random();
+        int randomNumber = 0;
+        System.out.println(randomNumber);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                randomNumber = random.nextInt(2);
+                System.out.print(randomNumber + " ");
+            }
+            System.out.println(randomNumber);
+        }
+
     }
 }
