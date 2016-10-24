0b5d53cfee18833ae5dad36f00259eb2a3ced2a2
Gboz <gbolesta21@gmail.com>
2016-10-24 22:03:02 +0200

    Generowanie populacji

--- src/com/gboz/javaworkout/FRastragina.java
+++ src/com/gboz/javaworkout/FRastragina.java
@@ -6,5 +6,38 @@ import java.util.Random;
  * Created by Grzegorz on 07.10.2016.
  */
 public class FRastragina {
+    double A = 10;
+    double w = 20 * Math.PI;
+    int k = 20;
+    double a = -1.0;
+    double b = 1.0;
+    double d = 6.0;
+    int m = 0;
+    String binary = "";
 
+    double rastriginFunction(double x, double A, double w) {
+        return A + Math.pow(x, 2) - A * Math.cos(w * x);
+    }
+
+    String generateBinary(int len) {
+        for (int i = 0; i < len; i++) {
+            binary += 0 + (int) (Math.random() * ((1 - 0) + 1));
+        }
+        return binary;
+    }
+
+    public int binaryToDecimal(String binary) {
+        char[] numbers = binary.toCharArray();
+        int val = 0;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] == '1') {
+                val += Math.pow(2, (numbers.length - i - 1));
+            }
+        }
+        return val;
+    }
+
+    double translate(int num, double a, double b, int m) {
+        return a + (b - a) * num / (Math.pow(2, m) - 1);
+    }
 }
--- src/com/gboz/javaworkout/Main.java
+++ src/com/gboz/javaworkout/Main.java
@@ -1,43 +1,59 @@
 package com.gboz.javaworkout;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
 
-    //metoda o zmiennej liczbie argumentów
-    static void varargs(int ... v) {
-        for (int x : v) {
-            System.out.print(x + " ");
+    public static String generateBinary(int len) {
+        String binary = "";
+        for (int i = 0; i < len; i++) {
+            binary += 0 + (int) (Math.random() * ((1 - 0) + 1));
         }
+        return binary;
+    }
+
+    public static int binaryToDecimal(String binary) {
+        char[] numbers = binary.toCharArray();
+        int val = 0;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] == '1') {
+                val += Math.pow(2, (numbers.length - i - 1));
+            }
+        }
+        return val;
+    }
+
+    public static double rastriginFunction(double x, double A, double w) {
+        return A + Math.pow(x, 2) - A * Math.cos(w * x);
+    }
+
+    public static double translate(int num, double a, double b, int m) {
+        return a + (b - a) * num / (Math.pow(2, m) - 1);
     }
 
     public static void main(String[] args) {
-//        System.out.println("Use method from class KeyWords");
-//        KeyWords var = new KeyWords();
-//        var.KeyWordsOutput(var.keyWords);
+        double A = 10, w = 20 * Math.PI, a = -1.0, b = 1.0, d = 6.0;
+        int k = 3, m = 0;
 
-        //metody globalizacji
-        System.out.print("coordinate n: ");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.print("coordinate m: ");
-        int m = scanner.nextInt();
-
-        Random random = new Random();
-        int randomNumber = 0;
-        System.out.println(randomNumber);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                randomNumber = random.nextInt(2);
-                System.out.print(randomNumber + " ");
-            }
-            System.out.println(randomNumber);
+        double value = (b - a) * Math.pow(10, d);
+        while (value >= Math.pow(2, m)) {
+            m++;
         }
 
-        //java kompendium
-        //zmienna liczba argumentów
-
-//        varargs(1, 5, 151353);
+        System.out.println("m: " + m);
+        String binarySeq[] = new String[k];
+        double doubleSeq[] = new double[k];
+        System.out.println("ciągi binarne: ");
+        for (int i = 0; i < k; i++) {
+            binarySeq[i] = generateBinary(m);
+            doubleSeq[i] = translate(binaryToDecimal(binarySeq[i]), a, b, m);
+            System.out.println(binarySeq[i]);
+        }
+        System.out.println("punkty po przekształceniu");
+        for (int i = 0; i < k; i++) {
+            System.out.println(doubleSeq[i]);
+        }
+        System.out.println("wartości funkcji Rastrigina");
+        for (int i = 0; i < k; i++) {
+            System.out.println("x" + i + " = " + rastriginFunction(doubleSeq[i], A, w));
+        }
     }
 }
