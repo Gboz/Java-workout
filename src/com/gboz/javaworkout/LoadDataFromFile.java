843ba8d889fc3703df2157bb414bd1adfc1eb1e9
Gboz <gbolesta21@gmail.com>
2016-10-27 17:40:16 +0200

    next fix in LoadDataFromFile.java

--- src/com/gboz/javaworkout/LoadDataFromFile.java
+++ src/com/gboz/javaworkout/LoadDataFromFile.java
@@ -2,16 +2,12 @@ package com.gboz.javaworkout;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/**
- * Created by Grzegorz on 26.10.2016.
- */
+
 public class LoadDataFromFile {
 
-    public static void loadFromFile() throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("C:\\Users\\Grzegorz\\IdeaProjects\\Java workout\\src\\com\\gboz\\javaworkout\\dane3.txt"));
         StringTokenizer stringTokenizer = new StringTokenizer(scanner.nextLine(), " ");
         ArrayList<Integer> integers = new ArrayList<>();
@@ -26,7 +22,7 @@ public class LoadDataFromFile {
         try {
             for (int i = 0; i <= count + 1; i++){
                 for (int j = 1; j <= 3; j++) {
-                    if (i == 0) continue;
+                    if (i == 0 || i == 1) continue;
                     while (stringTokenizer.hasMoreTokens()) {
                         x = Integer.parseInt(stringTokenizer.nextToken());
                         integers.add(x);
@@ -36,15 +32,16 @@ public class LoadDataFromFile {
                 }
                 stringTokenizer = new StringTokenizer(scanner.nextLine());
             }
-
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Koniec elementów: " + e);
         }
 
-    }
+        System.out.println(integers);
+        System.out.println(integers.get(0));
+        System.out.println(integers.get(1));
+        System.out.println(integers.get(2));
+        System.out.println(integers.get(4));
 
-    public static void main(String[] args) throws FileNotFoundException {
-        loadFromFile();
     }
 
 }
